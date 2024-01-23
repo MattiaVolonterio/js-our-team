e4e8bd3d8831e18897de42edca977ab9b70f26c8
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-23 14:58:19 +0100

    feat: print member info on html

--- js/main.js
+++ js/main.js
@@ -1,10 +1,16 @@
+// ## Recupero elementi dal DOM
+const stringResult = document.getElementById("string-result");
+
+// ## Ciclo l'array e recuper i dati
 for (let member of team) {
   let memberName = member.name;
   let memberRole = member.role;
   let memberImg = member.image;
-  console.log("------------------");
-  console.log(memberName);
-  console.log(memberRole);
-  console.log(memberImg);
-  console.log("------------------");
+
+  stringResult.innerHTML += `
+  Il nome del membro è: ${memberName}, 
+  il suo ruolo è: ${memberRole} 
+  e la sua immagine è: ${memberImg} 
+  </br>
+  </br>`;
 }
