5c16a3be2169ad068f393893b90e11bf22b0827c
MattiaVolonterio <volonteriomattia@gmail.com>
2024-01-23 14:48:17 +0100

    feat: print in console every team member info

---
+++ js/main.js
@@ -0,0 +1,10 @@
+for (let member of team) {
+  let memberName = member.name;
+  let memberRole = member.role;
+  let memberImg = member.image;
+  console.log("------------------");
+  console.log(memberName);
+  console.log(memberRole);
+  console.log(memberImg);
+  console.log("------------------");
+}
